ee51f065a52fc44ab3408d09543db4c836ef9495
eruvira <pretty.elf@mail.ru>
2022-11-10 13:36:37 +0600

    init commit

---
+++ src/components/GameCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="game-card">
+        <div class="game-card__image">
+            <img src="../assets/icons/apple-logo-svgrepo-com.svg"/>
+        </div>
+        <div class="description">
+            <div class="description__content">
+                <div class="description__platforms">
+                    <div class="description__linux">
+                    </div>
+                    <div class="description__apple">
+                    </div>
+                </div>
+                <div class="description__rating">5.5</div>
+            </div>
+            <h4 class="description__name">Cool Game Name</h4>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '../assets/styles/gameCard.css'
+</style>
---
+++ src/components/GamesPallete.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+import GameCard from './GameCard.vue'
+</script>
+
+<template>
+  <h1 class="head1">All Games</h1>
+  <div class="order-display">
+    <div class="order"></div>
+    <div class="display"></div>
+  </div>
+  <div class="games-pallete">
+      <GameCard/>
+      <GameCard/>
+      <GameCard/>
+      <GameCard/>
+      <GameCard/>
+      <GameCard/>
+      
+  </div>
+</template>
+
+<style scoped>
+
+</style>
